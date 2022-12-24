249dc4f8aa930bba4caddf8b1796f57d459e2654
1ShoukR <rahminshoukoohi@gmail.com>
2022-12-24 00:55:59 -0500

    cannot figure out bug with database. will ask adam

--- db/seeds.py
+++ db/seeds.py
@@ -12,26 +12,40 @@
 
 
 
-from flask import current_app
+from flask import Flask
 from datetime import datetime
-from ..models import User
+from models import User, db
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '\x14B~^\x07\xe1\x197\xda\x18\xa6[[\x05\x03QVg\xce%\xb2<\x80\xa4\x00'
+app.config['DEBUG'] = True
+
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Rahmin12@localhost:3306/joeva_remastered'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['DEBUG'] = True
+db.init_app(app)
 
 
 
-app_config = current_app.config
-
-
+# print(seed_data["username"])
 seed_data = {
-    "username": "guest",
-    "password": "guest",
-    "first_name": "guest",
-    "last_name": "guest",
-    "email": "guest@guest.com",
-    "created_at": datetime.utcnow,
-    "updated_at": datetime.utcnow
+    "username": " "
 }
 
-print(seed_data["username"])
 
 if User.query.filter_by(username=seed_data["username"]) is None:
+    seed_data = {
+        "username": "guest",
+        "password": "guest",
+        "first_name": "guest",
+        "last_name": "guest",
+        "email": "guest@guest.com",
+        "created_at": datetime.utcnow,
+        "updated_at": datetime.utcnow
+    }
+    seed_user_account = User(username=seed_data["username"], password=seed_data["password"], first_name=seed_data["first_name"], last_name=seed_data["last_name"], email=seed_data["email"], created_at=datetime.utcnow, updated_at=datetime.utcnow)
+    db.session.add(seed_user_account)
+    db.session.commit()
     User(**seed_data).save()
