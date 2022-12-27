51432800d2c6ab515b9ddb7e64be4be8572bf293
1ShoukR <rahminshoukoohi@gmail.com>
2022-12-26 21:19:29 -0500

    succesfully brought navbar to the main page

--- joeva-remastered/src/App.js
+++ joeva-remastered/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Homepage from './Components/Homepage';
+import Navbar from './Components/Navbar';
 import ReduxRoutes from './Components/ReduxRoutes';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <ReduxRoutes />
-      <Homepage />
     </div>
   );
 }
