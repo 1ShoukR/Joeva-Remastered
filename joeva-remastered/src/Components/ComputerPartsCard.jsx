ceb10d52bfdb1ff389f20505dfc253e61bbcd697
1ShoukR <rahminshoukoohi@gmail.com>
2022-12-29 20:07:14 -0500

    got the design flow of how item should be displayed on the screen

--- joeva-remastered/src/Components/ComputerPartsCard.jsx
+++ joeva-remastered/src/Components/ComputerPartsCard.jsx
@@ -6,11 +6,23 @@ export const ComputerPartsCard = (props) => {
     console.log("This is props", props)
   return (
     <div className="computerPartsCardContainer">
+        <div className="computerPartsCardName">
+            <p>{props?.item?.name}</p>
+        </div>
         <div className="computerPartsCardImageContainer">
             <img className="pcPartImage" src={props.item.image} alt="" />
         </div>
-        <div className="computerPartsCardName">
-            <p>{props?.item?.name}</p>
+        <div className="computerPartsCardPrice">
+            <p>{props?.item?.price}</p>
+        </div>
+        <div>
+            <p>{props?.item?.description}</p>
+        </div>
+        <div>
+            <p>{props?.item?.reviews}</p>
+        </div>
+        <div>
+            <button>Add to cart</button>
         </div>
     </div>
   );
