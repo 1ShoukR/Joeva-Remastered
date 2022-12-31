992ad1bbbc496657ece3de42c3ad706681ed190a
1ShoukR <rahminshoukoohi@gmail.com>
2022-12-30 21:54:28 -0500

    started editing the homepage a bit

--- joeva-remastered/src/Components/Homepage.jsx
+++ joeva-remastered/src/Components/Homepage.jsx
@@ -2,7 +2,14 @@ import React from "react";
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div className="homepageWrapper">
+      <div className="homepageHeaderContainer">
+        <h1>Welcome to Joeva!</h1>
+      </div>
+      <div>
+        <p>Browse all of our products on the page right here!</p>
+      </div>
+    </div>
   );
 };
 
